3107c0009c1e7557eec6d727dc35c6e0575b16b2
Asthenia182 <agembarzewska@gmail.com>
2021-02-02 22:57:48 +0100

    added js and package.json

---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,12 @@
+﻿console.log("Hello Pluralsight");
+
+var theForm = document.getElementById("TheForm");
+theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+    console.log("Buying Item");
+});
+
+var productInfo = document.getElementsByClassName("product-props");
+var listItems = productInfo.item[0].children;
